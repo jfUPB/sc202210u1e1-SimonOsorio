b1ee97af4be9a400d20815f5d08db58d7969ffa5
SimonOsorio <simonosoriouribe@gmail.com>
2022-02-10 10:49:57 -0500

    Avance 1

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,44 @@
 // SIMON OSORIO URIBE
 // 000420659
 // SIMON.OSORIOU@UPB.EDU.CO
-(START)
-    @START
+(RESET)
+    @SCREEN
+    D=A
+    @0
+    
+
+
+(KEYCHECK)
+//Tecla F = 70, Tecla C = 67
+    D=M
+    @
+
+(FILLCOLOR)
+    @1
+    M=-1 //Black
+    @FILLSCREEN
     0;JMP
+
+(CLEARCOLOR)
+    @1
+    M=0 //White
+    @FILLSCREEN
+
+(FILLSCREEN)
+    @1
+    D=M
+    @0
+    A=M
+    M=D
+    @0
+    D=M+1
+    @KBD
+    D=A-D
+    @0
+    M=M+1
+    A=M
+    @FILLSCREEN
+    D;JGT
+
+@RESET
+0;JMP
