f42d744bc5651fbb4a1b4b3d39ff6d7906234936
SimonOsorio <simonosoriouribe@gmail.com>
2022-02-10 21:05:10 -0500

    Updated Code

--- problem/program.asm
+++ problem/program.asm
@@ -1,11 +1,27 @@
 // SIMON OSORIO URIBE
 // 000420659
 // SIMON.OSORIOU@UPB.EDU.CO
-(RESET)
+(SETSCREEN)
     @SCREEN
     D=A
     @0
     M=D
+
+(FILLSCREEN)
+    @1
+    D=M
+    @0
+    A=M
+    M=D
+    @0
+    D=M+1
+    @KBD
+    D=A-D
+    @0
+    M=M+1
+    A=M
+    @FILLSCREEN
+    D;JGT
 
 (KEYCHECK)
 //Tecla F = 70, Tecla C = 67
@@ -28,31 +44,15 @@
 
 (FILLCOLOR)
     @1
-    M=-1 //Black
+    M=-1 //Black code
     @FILLSCREEN
     0;JMP
 
 (CLEARCOLOR)
     @1
-    M=0 //White
+    M=0 //White code
     @FILLSCREEN
     0;JMP
 
-(FILLSCREEN)
-    @1
-    D=M
-    @0
-    A=M
-    M=D
-    @0
-    D=M+1
-    @KBD
-    D=A-D
-    @0
-    M=M+1
-    A=M
-    @FILLSCREEN
-    D;JGT
-
-@RESET
+@SETSCREEN
 0;JMP
